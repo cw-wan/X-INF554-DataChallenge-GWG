3ecbb510e96d7808f7c1747f70fd5c5ce9f467de
cw-wan <charles.w.wan@gmail.com>
2023-11-22 23:32:16 +0100

    update hyper param

--- configs/naive_roberta_config.py
+++ configs/naive_roberta_config.py
@@ -17,7 +17,7 @@ class DownStream:
     output_size = 1  # 1 for binary classification
     dev_train_ratio = 0.1
     batch_size = 32
-    learning_rate = 3e-5
+    learning_rate = 1e-5
     warm_up = 1
     total_epoch = 20
     decay = 1e-3
